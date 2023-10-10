1281180654edf3b8f64dad716cedb624a9760680
mithu <nilchirkutpoison@gmail.com>
2023-10-10 09:21:01 +0600

    work on view file and  home.html file added

---
+++ app/views.py
@@ -0,0 +1,60 @@
+from django.shortcuts import render
+from django.views import View
+from .models import Customer, Cart, Product, OrderPlaced
+
+
+class ProductHomeView(View):
+ def get(self, request):
+  topwears = Product.objects.filter(category='TW')
+  bottomwears = Product.objects.filter(category='BW')
+  mobiles = Product.objects.filter(category='M')
+
+  context = {'topwears': topwears, 'bottomwears':bottomwears, 'mobiles':mobiles}
+  return render(request, 'app/home.html', context)
+
+
+
+class ProductDetailView(View):
+ def get(self, request, pk):
+  product = Product.objects.get(pk=pk)
+  return render(request, 'app/productdetail.html', {'product':product})
+
+
+def add_to_cart(request):
+ return render(request, 'app/addtocart.html')
+
+def buy_now(request):
+ return render(request, 'app/buynow.html')
+
+def profile(request):
+ return render(request, 'app/profile.html')
+
+def address(request):
+ return render(request, 'app/address.html')
+
+def orders(request):
+ return render(request, 'app/orders.html')
+
+def change_password(request):
+ return render(request, 'app/changepassword.html')
+
+
+
+def mobile(request, data=None):
+ if data == None:
+  mobiles = Product.objects.filter(category='M')
+ elif data == 'Apple' or data == 'Samsung':
+  mobiles = Product.objects.filter(category='M').filter(brand=data)
+
+ return render(request, 'app/mobile.html', {'mobiles':mobiles})
+
+
+
+def login(request):
+ return render(request, 'app/login.html')
+
+def customerregistration(request):
+ return render(request, 'app/customerregistration.html')
+
+def checkout(request):
+ return render(request, 'app/checkout.html')

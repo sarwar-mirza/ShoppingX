76ad1627fdd2a2caa9d5e8600266a8c58eb8823d
mithu <nilchirkutpoison@gmail.com>
2023-10-15 16:27:59 +0600

    add-to-cart  added

--- app/urls.py
+++ app/urls.py
@@ -14,7 +14,11 @@ urlpatterns = [
 
     path('product-detail/<int:pk>/', views.ProductDetailView.as_view(), name='product-detail'),
 
-    path('cart/', views.add_to_cart, name='add-to-cart'),
+    path('add-to-cart/', views.add_to_cart, name='add-to-cart'),
+    
+    path('cart/', views.show_cart, name='showcart'),
+
+
     path('buy/', views.buy_now, name='buy-now'),
 
 
--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from .models import Customer, Cart, Product, OrderPlaced
 from .forms import CustomerRegistrationForm, ProfileCustomerForm
@@ -24,7 +24,39 @@ class ProductDetailView(View):
 
 
 def add_to_cart(request):
- return render(request, 'app/addtocart.html')
+ user = request.user
+ product_id = request.GET.get('prod_id')
+ product = Product.objects.get(id=product_id)
+
+ Cart(user=user, product=product).save()
+
+ return redirect('/cart')
+
+
+
+def show_cart(request):
+ if request.user.is_authenticated:
+  user = request.user
+  cart = Cart.objects.filter(user=user)
+
+  amount = 0.0
+  shipping_amount = 70.0
+  total_amount = 0.0
+  cart_product = [p for p in Cart.objects.all() if p.user == user]
+
+  if cart_product:
+   for p in cart_product:
+    tempamount = (p.quantity  *  p.product.discounted_price)
+    amount += tempamount
+    totalamount = amount + shipping_amount
+   return render(request, 'app/addtocart.html', {'carts':cart, 'totalamount':totalamount, 'amount':amount})
+  else:
+   return render(request, 'app/emptycart.html')
+   
+
+ 
+
+
 
 def buy_now(request):
  return render(request, 'app/buynow.html')
